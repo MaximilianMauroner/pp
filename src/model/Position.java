0463a6f4700cc9a813f358246f7c99ae46a55b98
Maximilian Mauroner <github@relay.mauroner.eu>
2023-10-10 17:07:43 +0200

    added getters for position

--- src/model/Position.java
+++ src/model/Position.java
@@ -2,4 +2,18 @@ package src.model;
 
 public class Position {
     int x, y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
 }
