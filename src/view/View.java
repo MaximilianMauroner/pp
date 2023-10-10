5ed7b152fab75afbe988e870294b37873c3e195c
BuildTools <unconfigured@null.spigotmc.org>
2023-10-10 17:28:52 +0200

    initial commit

--- src/view/View.java
+++ src/view/View.java
@@ -1,13 +1,21 @@
 package src.view;
 
+import codedraw.CodeDraw;
 import src.model.Point;
 
 import java.util.List;
 
 public class View {
-    public View(){
+    private int width, height;
+    private final CodeDraw cd;
+
+    public View(int width, int height) {
+        this.width = width;
+        this.height = height;
+        cd = new CodeDraw(width, height);
     }
 
-    public void draw(List<Point> points){
+    public void draw(List<Point> points) {
+        cd.show();
     }
 }
