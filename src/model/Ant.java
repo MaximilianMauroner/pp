db23164f2e306b6037ff40d6dddd2e28348e5e26
Lessi <44904007+LeskovarLukas@users.noreply.github.com>
2023-10-10 17:44:27 +0200

    adding ant state

--- src/model/Ant.java
+++ src/model/Ant.java
@@ -3,6 +3,14 @@ package src.model;
 import src.controller.GameplayLoop;
 
 public class Ant extends Entity {
+    private AntState currentState = AntState.EXPLORE;
 
+    public void setState(AntState state) {
+        this.currentState = state;
+    }
+
+    public AntState getState() {
+        return this.currentState;
+    }
 
 }
