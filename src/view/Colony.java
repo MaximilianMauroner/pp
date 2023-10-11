ce65df15ecb4dd91605c7e684aab29e3b9d0d8e6
BuildTools <unconfigured@null.spigotmc.org>
2023-10-11 16:02:00 +0200

    View prototype 3

---
+++ src/view/Colony.java
@@ -0,0 +1,13 @@
+package src.view;
+
+import src.controller.GameState;
+import src.model.Entity;
+import src.model.Point;
+import src.model.Status;
+
+public class Colony implements Entity {
+    @Override
+    public void run(GameState gameState, Status status, Point point) {
+
+    }
+}
