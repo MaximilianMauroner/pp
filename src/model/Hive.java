3ee3b1d19df6c1b605efe67a5497687693133178
Lessi <44904007+LeskovarLukas@users.noreply.github.com>
2023-10-11 15:52:01 +0200

    add classes

---
+++ src/model/Hive.java
@@ -0,0 +1,11 @@
+package src.model;
+
+import src.controller.GameState;
+
+public class Hive implements Entity {
+
+    @Override
+    public void run(GameState gameState, Status status, Point point) {
+
+    }
+}
---
+++ src/model/Obstacle.java
@@ -0,0 +1,10 @@
+package src.model;
+
+import src.controller.GameState;
+
+public class Obstacle implements Entity {
+    @Override
+    public void run(GameState gameState, Status status, Point point) {
+
+    }
+}
