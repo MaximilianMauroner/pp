bc2d4119810902a2a92b595b67ab7567d671fa4f
BuildTools <unconfigured@null.spigotmc.org>
2023-10-18 16:11:19 +0200

    change time completed

--- src/Aufgabe1-3/view/View.java
+++ src/Aufgabe1-3/view/View.java
@@ -10,6 +10,9 @@ import java.util.concurrent.ConcurrentHashMap;
 public class View {
     private final int width, height;
     private final CodeDraw cd;
+    private Color BACKGROUND_COLOR = Color.BLACK;
+    private int time = 0;
+    private int hourOfTheDay = 0;
 
     public View(int width, int height) {
         this.width = width * Parameters.SCALE_BY;
@@ -25,10 +28,7 @@ public class View {
      * @param gameState gameState to draw on the canvas. The gameState must include a Point[] with all Entities. If the array is empty, then nothing will be drawn.
      */
     public void draw(GameState gameState) {
-        resetCanvas();
         drawElements(gameState);
-
-
         cd.show();
     }
 
@@ -40,6 +40,9 @@ public class View {
      * @param gameState gameState to draw on the canvas
      */
     private void drawElements(GameState gameState) {
+        time++;
+        System.out.println(time);
+        if (time % 20 == 0) changeTime();
 
         ConcurrentHashMap<Position, Point> points = gameState.getPoints();
         for (Point point : points.values()) {
@@ -47,16 +50,7 @@ public class View {
             int y = point.getPosition().getY() * Parameters.SCALE_BY;
 
             for (Entity entity : point.getEntities()) {
-                if (entity instanceof Trail e) {
-                    double strength = e.getStrength();
-                    if (e.getStrength() > 1) {
-                        strength = 1;
-                    }
-                    setPixels(x, y, Parameters.SCALE_BY, new Color(
-                            (int) (Parameters.TRAIL_COLOR.getRed() * strength),
-                            (int) (Parameters.TRAIL_COLOR.getGreen() * strength),
-                            (int) (Parameters.TRAIL_COLOR.getBlue() * strength)));
-                }
+                if (entity instanceof Trail e) setPixels(x, y, Parameters.SCALE_BY, mixColors(Parameters.TRAIL_COLOR, BACKGROUND_COLOR, (float) e.getStrength()));
                 if (entity instanceof Food) setPixels(x, y, Parameters.SCALE_BY, Parameters.FOOD_SOURCE_COLOR);
                 if (entity instanceof Hive) setPixels(x, y, Parameters.SCALE_BY, Parameters.COLONY_HOME_COLOR);
                 if (entity instanceof Obstacle) setPixels(x, y, Parameters.SCALE_BY, Parameters.OBSTACLE_COLOR);
@@ -72,14 +66,63 @@ public class View {
     }
 
     /**
-     * Reset the canvas to black
+     * mixColors mixes two colors with the given strength. The strength must be between 0 and 1.
+     * The strength is the percentage of the color. The rest is the percentage of the backgroundColor.
+     * If the strength is 0, the backgroundColor will be returned. If the strength is 1, the color will be returned.
+     * If the strength is between 0 and 1, the color will be mixed with the backgroundColor.
+     *
+     * @param color           color to mix with the backgroundColor
+     * @param backgroundColor Background color. Color to mix with the foreground color
+     * @param strength        strength of the color. Must be between 0 and 1
+     * @return Returnes the mixed color of the two colors
      */
-    private void resetCanvas() {
-        setPixels(width / 2, height / 2, width, Color.BLACK);
+    private Color mixColors(Color color, Color backgroundColor, float strength) {
+        assert strength >= 0 && strength <= 1;
+
+        try {
+            return new Color(
+                    (int) (color.getRed() * strength + ((1 - strength) * backgroundColor.getRed())),
+                    (int) (color.getGreen() * strength + ((1 - strength) * backgroundColor.getGreen())),
+                    (int) (color.getBlue() * strength + ((1 - strength) * backgroundColor.getBlue())));
+        } catch (Exception e) {
+            System.out.println("r:" + (int) (color.getRed() * strength + ((1 - strength) * backgroundColor.getRed())));
+            System.out.println("g:" + (int) (color.getGreen() * strength + ((1 - strength) * backgroundColor.getGreen())));
+            System.out.println("b:" + (int) (color.getBlue() * strength + ((1 - strength) * backgroundColor.getBlue())));
+            System.out.println();
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * changeTime: Change the time of the day. The background color will be changed.
+     * The background color will be changed from black to white and from white to black.
+     * A daylight cycle will be simulated.
+     * <p>
+     * Starts with Daytime by default.
+     * If it is desired to start with NightTime, then replace the < with >=
+     */
+    private void changeTime() {
+        hourOfTheDay++;
+        if (hourOfTheDay % 24 < 12) {
+            BACKGROUND_COLOR = mixColors(Color.BLACK, Color.WHITE, (float) (hourOfTheDay % 12) / 12);
+        } else {
+            BACKGROUND_COLOR = mixColors(Color.WHITE, Color.BLACK, (float) (hourOfTheDay % 12) / 12);
+        }
+
+        changeBackgroundColor();
+    }
+
+    /**
+     * changeBackgroundColor: Change the background color of the canvas to whatever is set in the BACKGROUND_COLOR variable at the moment.
+     */
+    private void changeBackgroundColor() {
+        setPixels(width / 2, height / 2, width, BACKGROUND_COLOR);
     }
 
     /**
      * Set pixels in a square around the given coordinates
+     * In essence this is a square with the size "size" around the coordinates (x,y)
      *
      * @param x     x coordinate of the center. If x is bigger than the width, it will be set to the width.
      * @param y     y coordinate of the center. If y is bigger than the height, it will be set to the height.
@@ -92,6 +135,7 @@ public class View {
         if (y < 0) y = 0;
         if (y > height) y = height;
 
+        //go size/2 times to the left, right, up and down from the center and fill every pixel with the given color
         for (int i = x - size / 2; i <= x + size / 2; i++) {
             for (int j = y - size / 2; j <= y + size / 2; j++) {
                 cd.setPixel(i, j, color);
