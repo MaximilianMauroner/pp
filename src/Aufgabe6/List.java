c5a855c4304ed01768e1df3007ca4e629253cf13
Lessi <44904007+LeskovarLukas@users.noreply.github.com>
2023-11-21 16:31:33 +0100

    addin GENERIC list

--- src/Aufgabe6/List.java
+++ src/Aufgabe6/List.java
@@ -1,2 +1,59 @@
-public class List<X> {
+public class List<T> {
+    private List<T> next;
+    private T value;
+
+    public List(T value) {
+        this.value = value;
+    }
+
+    public void add(T value) {
+        if (identical(value)) {
+            return;
+        }
+
+        List<T> last = next;
+        List<T> curr = next.next;
+        while (curr != null) {
+            last = curr;
+            curr = curr.next;
+        }
+        last.next = new List<>(value);
+    }
+
+    public boolean contains(T value) {
+        List<T> curr = next;
+        while (curr != null) {
+            if (curr.value.equals(value)) {
+                return true;
+            }
+            curr = curr.next;
+        }
+        return false;
+    }
+
+    public boolean identical(T value) {
+        List<T> curr = next;
+        while (curr != null) {
+            if (curr.value == value) {
+                return true;
+            }
+            curr = curr.next;
+        }
+        return false;
+    }
+
+    public boolean remove(T value) {
+        List<T> curr = next;
+        List<T> last = next;
+        while (curr != null) {
+            if (curr.value.equals(value)) {
+                last.next = curr.next;
+                return true;
+            }
+            last = curr;
+            curr = curr.next;
+        }
+
+        return false;
+    }
 }
