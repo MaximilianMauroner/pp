eb48acd7f60472591bdd73d715548e66bfb0d9ba
Maximilian Mauroner <github@relay.mauroner.eu>
2023-12-05 17:03:34 +0100

    finished tests
    Took 6 minutes

--- src/Aufgabe7/Test.java
+++ src/Aufgabe7/Test.java
@@ -5,14 +5,14 @@ import Formicarium.*;
 public class Test {
     public static void main(String[] args) {
 
-        testFornicarium();
-
-        // Test 5 - Show Formicarium & Show Ants
-
-        // Test 6 - Add Ant Colony & Remove Ant Colony
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Test Instance " + i);
+            testFornicarium(i);
+            System.out.println("Test Instance " + i + " finished\n\n");
+        }
     }
 
-    private static void testFornicarium() {
+    private static void testFornicarium(int instance) {
         Institute i = new Institute();
         Formicarium f1 = new LargeUnconditionedFormicarium(1);
         Formicarium f2 = new MediumUnconditionedFormicarium(20);
@@ -110,11 +110,15 @@ public class Test {
         i.removeAntColony(ac1);
         testEquals(i.containsAnt(ac1), false);
 
-        System.out.println("Test 7 - Visitor");
-        String expectedBefore = "Institute.assignForm(AntColony): 30\nFormicarium.LargeConditionedFormicarium.accept(AntColony): 6\nFormicarium.MediumConditionedFormicarium.antType(): 6\nFormicarium.LargeUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.MediumConditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeUnconditionedFormicarium.antType(): 7\nFormicarium.MediumConditionedFormicarium.setAntType(AntColony): 4\nFormicarium.MediumUnconditionedFormicarium.antType(): 6\nFormicarium.SmallUnconditionedFormicarium.setAntType(AntColony): 2\nFormicarium.MediumUnconditionedFormicarium.setAntType(AntColony): 4\nFormicarium.MediumUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeConditionedFormicarium.antType(): 6\nFormicarium.SmallUnconditionedFormicarium.antType(): 6\nFormicarium.LargeUnconditionedFormicarium.setAntType(AntColony): 4\nFormicarium.SmallUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeConditionedFormicarium.setAntType(AntColony): 4\n".formatted();
-        testEquals(before, expectedBefore);
-        String expectedAfter = "Institute.assignForm(AntColony): 36\nFormicarium.LargeConditionedFormicarium.accept(AntColony): 6\nFormicarium.MediumConditionedFormicarium.antType(): 6\nFormicarium.LargeUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.SmallConditionedFormicarium.accept(AntColony): 6\nFormicarium.MediumConditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeUnconditionedFormicarium.antType(): 7\nFormicarium.MediumConditionedFormicarium.setAntType(AntColony): 4\nFormicarium.MediumUnconditionedFormicarium.antType(): 6\nFormicarium.SmallUnconditionedFormicarium.setAntType(AntColony): 2\nFormicarium.MediumUnconditionedFormicarium.setAntType(AntColony): 4\nFormicarium.SmallConditionedFormicarium.antType(): 6\nFormicarium.MediumUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeConditionedFormicarium.antType(): 6\nFormicarium.SmallConditionedFormicarium.setAntType(AntColony): 2\nFormicarium.SmallUnconditionedFormicarium.antType(): 6\nFormicarium.LargeUnconditionedFormicarium.setAntType(AntColony): 4\nFormicarium.SmallUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeConditionedFormicarium.setAntType(AntColony): 4\n".formatted();
-        testEquals(after, expectedAfter);
+
+        // only needs to be tested once
+        if (instance == 1) {
+            System.out.println("Test 7 - Visitor");
+            String expectedBefore = "Institute.assignForm(AntColony): 30\nFormicarium.LargeConditionedFormicarium.accept(AntColony): 6\nFormicarium.MediumConditionedFormicarium.antType(): 6\nFormicarium.LargeUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.MediumConditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeUnconditionedFormicarium.antType(): 7\nFormicarium.MediumConditionedFormicarium.setAntType(AntColony): 4\nFormicarium.MediumUnconditionedFormicarium.antType(): 6\nFormicarium.SmallUnconditionedFormicarium.setAntType(AntColony): 2\nFormicarium.MediumUnconditionedFormicarium.setAntType(AntColony): 4\nFormicarium.MediumUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeConditionedFormicarium.antType(): 6\nFormicarium.SmallUnconditionedFormicarium.antType(): 6\nFormicarium.LargeUnconditionedFormicarium.setAntType(AntColony): 4\nFormicarium.SmallUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeConditionedFormicarium.setAntType(AntColony): 4\n".formatted();
+            testEquals(before, expectedBefore);
+            String expectedAfter = "Institute.assignForm(AntColony): 36\nFormicarium.LargeConditionedFormicarium.accept(AntColony): 6\nFormicarium.MediumConditionedFormicarium.antType(): 6\nFormicarium.LargeUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.SmallConditionedFormicarium.accept(AntColony): 6\nFormicarium.MediumConditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeUnconditionedFormicarium.antType(): 7\nFormicarium.MediumConditionedFormicarium.setAntType(AntColony): 4\nFormicarium.MediumUnconditionedFormicarium.antType(): 6\nFormicarium.SmallUnconditionedFormicarium.setAntType(AntColony): 2\nFormicarium.MediumUnconditionedFormicarium.setAntType(AntColony): 4\nFormicarium.SmallConditionedFormicarium.antType(): 6\nFormicarium.MediumUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeConditionedFormicarium.antType(): 6\nFormicarium.SmallConditionedFormicarium.setAntType(AntColony): 2\nFormicarium.SmallUnconditionedFormicarium.antType(): 6\nFormicarium.LargeUnconditionedFormicarium.setAntType(AntColony): 4\nFormicarium.SmallUnconditionedFormicarium.accept(AntColony): 6\nFormicarium.LargeConditionedFormicarium.setAntType(AntColony): 4\n".formatted();
+            testEquals(after, expectedAfter);
+        }
 
     }
 
