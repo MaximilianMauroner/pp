97b142a7a0db75522e7cece90f42ebc5f5e35a31
Lessi <44904007+LeskovarLukas@users.noreply.github.com>
2023-12-08 14:28:41 +0100

    Changes by maxim

--- src/Aufgabe8/Test.java
+++ src/Aufgabe8/Test.java
@@ -1,6 +1,9 @@
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) {
@@ -20,6 +23,14 @@ public class Test {
             nodes.get(3), List.of(nodes.get(4)),
             nodes.get(4), null
         );
+        // (0,1) (1, 2) (2,3)
+        //(0, (1, 2, 3))
+
+        // kleinerer Index zuerst
+        // i = 0, j = 1
+        // i -> j (i, j)
+        // i = 0, j = 1
+        // j -> i (i, j)
 
         // initialize distance list
         List<Distance> distances = new ArrayList<>();
@@ -37,6 +48,18 @@ public class Test {
                 intensities.add(new Intensity(i, j, 0));
             });
         });
+
+//        List<Iteration> iterations;
+//        Stream.iterate(0, i -> i + 1)
+//                .limit(10)
+//                .map(i -> {
+//                    
+//                    Iteration next = iterations.get(i).next();
+//                    iterations.add(next);
+//                })
+//                .toList();
+
+
 
 //        positions p = ants.foreach(ant
 //                ant.move()
