047332966192a0bd445ad8ac237debfa52824f28
Lessi <44904007+LeskovarLukas@users.noreply.github.com>
2023-12-14 18:10:19 +0100

    Changes by maxim

--- src/Aufgabe8/Ant.java
+++ src/Aufgabe8/Ant.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 public class Ant {
     public Ant(int ID, int node) {
@@ -11,6 +14,49 @@ public class Ant {
     public final Stack<Integer> visited = new Stack<>();
 
     public Double move(Iteration iter, BiFunction<Iteration, Ant, Integer> selector) {
+
+        Integer current = visited.peek();
+
+
+
+
+        List<Distance> neighborDist = iter.graph.distances.stream()
+                .filter(distance -> distance.i == current || distance.j == current)
+                .filter(distance -> !visited.contains(distance.i) || !visited.contains(distance.j))
+                .toList();
+
+        List<Intensity> intensities = new JoinChanges().apply(iter.intensities, iter.changeBuffer);
+        List<Intensity> neighborIntensity = intensities.stream()
+                .filter(intensity -> intensity.i == current || intensity.j == current)
+                .filter(intensity -> !visited.contains(intensity.i) || !visited.contains(intensity.j))
+                .toList();
+
+
+
+
+        neighborIntensity.forEach(intensity -> {
+            ProbabilityRecord pr = new ProbabilityRecord(
+                    intensities.stream()
+                            .filter(intensity::equals)
+                            .findFirst().orElse(null),
+                    neighborDist.stream()
+                            .filter(item -> item.i == intensity.i && item.j == intensity.j)
+                            .findFirst().orElse(null),
+                    Test.ALPHA,
+                    Test.BETA,
+                    intensities.stream()
+                            .filter(item -> !intensity.equals(item))
+                            .toList(),
+                    neighborDist.stream()
+                            .filter(item -> item.i != intensity.i && item.j != intensity.j)
+                            .collect(Collectors.toList())
+            );
+        });
+
+
+        List<ProbabilityRecord> list = new ArrayList<>();
+
+//
 //        int nextNode = selector.apply(iter, this);
 //        node.push(nextNode);
         return 1.0;
